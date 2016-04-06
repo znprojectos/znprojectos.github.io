7dd36c5650fce9b1b49e5e18286858ec510cf898
nuno <znprojectos@gmail.com>
2016-04-06 15:13:07 +0100

    Qua Abr  6 15:13:07 WEST 2016

---
+++ main.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+
+# Does everything AND commits page
+#
+# This requires setup:
+# 
+# git config credential.helper store
+# git push https://github.com/znprojectos/znprojectos.github.io.git
+# git config --global credential.helper 'cache --timeout 777200'
+
+./go_news.sh
+git add *
+git commit -m "`date`"
+git push -u origin master
