a9d45fbd36c4f43c4b9030cc419e7c2a53f9168d
znprojectos <znprojectos@gmail.com>
2016-09-24 02:01:19 +0100

    Sat 24 Sep 02:01:19 WEST 2016

--- gen_feed_html.sh
+++ gen_feed_html.sh
@@ -9,6 +9,14 @@ function do_header
 			<meta charset=\"UTF-8\">
 			<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
 			<link rel=\"stylesheet\" href=\"bootstrap.min.css\">
+			<script>
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+			ga('create', 'UA-84384557-1', 'auto');
+			ga('send', 'pageview');
+			</script>
 			</head>
 			<body>
 			`date +%d/%m/%Y-%R`
